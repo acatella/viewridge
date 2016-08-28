708885e3d8c810d5928462af79155f54ccac143a
acatella <aficclark@gmail.com>
2016-08-28 12:33:46 -0700

    first commit, installed angular webapp, configured grunt-sass

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name viewridgeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the viewridgeApp
+ */
+angular.module('viewridgeApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
